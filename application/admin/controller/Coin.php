68f62a0a4ff9822a1bbac9411fdf2baf3eae365b
administritorWu <wuchang653@163.com>
2019-10-27 21:57:47 +0800

    修改coin

--- application/admin/controller/Coin.php
+++ application/admin/controller/Coin.php
@@ -1,17 +1,9 @@
 <?php
 namespace app\admin\controller;
-
-use app\admin\model\User as Users;
-
 class User
 {
     public function test()
     {
-        $list = Users::where(['id'=>1])->value('username');
-        dump($list);
-        exit;
-
-        $str = '111';
-        return view('admin/User/UserList',['title'=>'会员列表','str'=>$str]);
+        echo 1;
     }
 }
